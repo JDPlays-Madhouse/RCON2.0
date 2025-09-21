0626fb49fc06bcfb9c243053e469e7912fe7d3d6
Ozy-Viking <admin@hankin.io>
2025-09-21 14:34:00 +1000

    fix: Eslint causing build to fail.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,15 +6,28 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends("next/core-web-vitals", "next/typescript"), {
+export default [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
     rules: {
-        "@typescript-eslint/no-empty-object-type": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-        "react-hooks/exhaustive-deps": "off"
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
     },
-}];
+  },
+  {
+    ignores: [
+      "out/**",
+      "src-tauri/**",
+      "eslint.config.mjs",
+      "example/**",
+      "next-env.d.ts",
+      ".next/**",
+    ],
+  },
+];
