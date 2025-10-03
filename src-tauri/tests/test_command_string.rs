b10c79511219be71202ffcf9a14d2ca238081248
Ozy-Viking <admin@hankin.io>
2025-10-03 17:53:29 +1000

    fix(test): Failing test

--- src-tauri/tests/test_command_string.rs
+++ src-tauri/tests/test_command_string.rs
@@ -18,6 +18,6 @@ fn file_command_print() {
         lua_command,
         variables,
     };
-    let expected = "/silent-command local player_name = \"JD-Plays\";\nlocal radius = 25;local player = game.get_player(player_name);if player then; local force = player.force;for k,v in pairs(player.surface.find_entities_filtered{position=player.position, radius=radius}) do; v.order_deconstruction(force);end;end;";
+    let expected = "/silent-command local player_name = \"JD-Plays\";\nlocal radius = 25;local player = game.get_player(player_name);if player then;local force = player.force; for k, v in pairs(player.surface.find_entities_filtered { position = player.physical_position, radius = radius }) do v.order_deconstruction(force); end; end;";
     assert_eq!(rcon_command.command(None, "test").as_str().trim(), expected);
 }
